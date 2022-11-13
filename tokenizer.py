ebf2ffc5937b771614fdeebb283f03cf041af42d
PaperDrago <117543984+PaperDrago@users.noreply.github.com>
2022-11-12 20:31:49 -0600

    Add files via upload
    Added Carlos' contribution to the final

--- tokenizer.py
+++ tokenizer.py
@@ -2,6 +2,8 @@ import abc
 import re
 from abc import ABC
 from typing import List
+from nltk.stem import PorterStemmer
+from nltk.stem import WordNetLemmatizer
 
 
 class Tokenizer(ABC):
@@ -10,6 +12,7 @@ class Tokenizer(ABC):
 
     Implemented as a part of HW2 exercise 3.
     """
+
     @abc.abstractmethod
     def tokenize(self, text: str) -> List[str]:
         pass
@@ -19,8 +22,31 @@ class NaiveTokenizer(Tokenizer):
     """
     Tokenizer implementation from HW1.
     """
+
     def tokenize(self, text: str) -> List[str]:
         initial = re.sub(r'(\W)', r' \1 ', text.lower())
         adjusted = re.sub(r'(\w) \' (\w)', r"\1'\2", initial)
         with_ellipses = re.sub(r'\.\s+\.\s+\.', '...', adjusted)
         return with_ellipses.split()
+
+
+class StemAndLem(Tokenizer):
+    def tokenize(self, text: str) -> List[str]:
+        initial = re.sub(r'(\W)', r' \1 ', text.lower())
+        adjusted = re.sub(r'(\w) \' (\w)', r"\1'\2", initial)
+        with_ellipses = re.sub(r'\.\s+\.\s+\.', '...', adjusted)
+        return with_ellipses.split()
+
+    def stemm(self, tokenized: List[str]) -> List[str]:
+        stem = PorterStemmer()
+        stemmed = list()
+        for word, i in tokenized:
+            stemmed[i] = stem.stem(word)
+        return stemmed
+
+    def lemm(self, tokenized: List[str]) -> List[str]:
+        lem = WordNetLemmatizer()
+        lemmed = list()
+        for word, i in tokenized:
+            lemmed[i] = lem.lemmatize(word)
+        return lemmed
